d1c44f65a44ffb7a93f7f4834dbffeb02662b5c6
jyun27 <kimjyun27@gmail.com>
2024-09-05 04:52:23 +0900

    section4(3). 테스트 케이스 작성 중

---
+++ src/main/java/sia/spring_board/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package sia.spring_board.repository;
+
+import sia.spring_board.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+}
---
+++ src/test/java/sia/spring_board/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package sia.spring_board.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public  void save(){
+
+    }
+}
